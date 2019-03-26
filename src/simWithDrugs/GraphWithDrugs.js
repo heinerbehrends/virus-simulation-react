c89e5fd0c62fa531bd45457a9373a4ac43c2a7cf
heinerbehrends <heinerbehrends@hotmail.com>
2019-03-26 21:14:58 +0100

    moved stuff to simWithDrugs and GraphWithDrugs

---
+++ src/simWithDrugs/GraphWithDrugs.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+const GraphWithDrugs = ({ arrays, layout }) => {
+  const makePlot = arr => (
+    {
+      x: Array.from(Array(arr.length).keys()),
+      y: arr,
+      mode: 'lines',
+    }
+  );
+  const makeData = arrays => (
+    arrays.map(
+      array => makePlot(array)
+    )
+  )
+  
+  return (
+    <Plot
+      data={ makeData(arrays) }
+      layout={ layout }
+    />
+  );
+};
+
+export default GraphWithDrugs;
---
+++ src/simWithDrugs/simWithDrugs.js
@@ -0,0 +1,40 @@
+import { runSimulation } from '../simpleSim/simpleSim';
+
+export const sim = patientWithDrugs => (
+  [
+    patientWithDrugs.updateViruses(),
+    [
+      patientWithDrugs.getVirusCount(),
+      patientWithDrugs.getResistentCount('guttagonol'),
+    ],
+  ]
+);
+
+const mergePairs = pairs => pairs.reduce(
+  (acc, pair) => [[...acc[0], pair[0]], [...acc[1], pair[1]]],
+  [[], []]
+);
+
+export function simulationWithDrugs({
+  func,
+  patient,
+  repetitions,
+}) {
+  const [newPatient, firstArray] = runSimulation({
+    func,
+    patient,
+    repetitions: repetitions / 2,
+  });
+    
+  return mergePairs(
+    firstArray.concat(
+      runSimulation({
+        func,
+        patient: newPatient.addDrug('guttagonol')      ,
+        repetitions: repetitions / 2,
+      })[1]
+    )
+  );
+};
+
+export default runSimulation;
